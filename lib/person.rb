14fb4a3a2141dc4cf7b42c0ce592be7bc08ca82f
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-08-25 09:28:12 +0300

    commit tree 5e73b5909d5b664b031fd4c725ebdcc24122ca42

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,70 @@
 # your code goes here
+class Person
+  attr_reader :name, :happiness, :hygiene
+  attr_accessor :bank_account, :happiness, :hygiene
+
+  def initialize(name)
+      @name = name
+      @bank_account = 25
+      @hygiene = 8
+      @happiness = 8
+
+  end
+
+  def hygiene=(num)
+      @hygiene = num
+      @hygiene = 10 if @hygiene > 10
+      @hygiene = 0 if @hygiene < 0
+  end
+
+  def happiness=(num)
+      @happiness = num
+      @happiness = 10 if @happiness > 10
+      @happiness = 0 if @happiness < 0
+  end
+
+  def clean?
+      @hygiene>7
+  end
+
+  def happy?
+      @happiness>7
+  end
+
+  def get_paid(amount)
+      self.bank_account += amount
+      self.happiness += 1
+      return "all about the benjamins"
+
+  end
+
+  def take_bath
+      self.hygiene +=4
+      "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+      self.hygiene -=3
+      self.happiness +=2
+      "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+      [friend, self].each {|o| o.happiness += 3 }
+      "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person,topic)
+      objects =[self,person]
+      case topic
+      when "politics"
+          objects.each { |o| o.happiness -= 2}
+          "blah blah partisan blah lobbyist"
+      when "weather"
+          objects.each { |o| o.happiness += 1}
+          "blah blah sun blah rain"
+      else
+          "blah blah blah blah blah"
+      end
+  end
+end     
